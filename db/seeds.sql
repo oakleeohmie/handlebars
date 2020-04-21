80dfb575eba10a513c1fd7de09cb051b40356520
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-04-20 18:21:21 -0600

    created schema

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES('Hamburger'),
+    ('Cheeseburger'),
+    ('Bacon Burger'),
+    ('Veggie Burger'),
+    ('Cajun Burger'),
+    ('Turkey Burger'),
+    ('BBQ Burger'),
+    ('Southwest Burger'),
+    ('Teriyaki Burger')
