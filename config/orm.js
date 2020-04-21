a2cfbf14da30460367206363d6e1ee7a35398fac
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-04-20 18:26:59 -0600

    created orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,24 +1,44 @@
-var mysql = require("mysql");
+let connection = require('./connection');
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+var orm = {
+    selectAll: function (cb) {
+        let queryString = "SELECT * FROM burgers;";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function (burger_name, cb) {
+        let queryString = "INSERT INTO burgers (burger_name) VALUES(?);";
+        connection.query(queryString, [burger_name], (err, res) => {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
 
-    var connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: process.env.MYSQL_PASSWORD,
-        database: "burgers_db"
-    });
+    update: function (id, cb) {
+        let queryString = "UPDATE burgers SET devoured = 1  WHERE id = ?;";
+        connection.query(queryString, [id], (err, res) => {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    delete: function (id, cb) {
+        let queryString = "DELETE FROM burgers WHERE id = ?";
+        connection.query(queryString, [id], (err, res) => {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
+
 };
 
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
-module.exports = connection;
+module.exports = orm;
